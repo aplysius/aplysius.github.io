14ddefeb8351811754c44b2ff4875ee434c3f8f5
aplysius <50696341+aplysius@users.noreply.github.com>
2021-09-11 11:01:21 +0200

    Add files via upload

---
+++ header.js
@@ -0,0 +1,11 @@
+document.write('\
+<!-- <a id="home" href="index.html"></a> -->\
+<h1>Dancing Matter</h1>\
+<h2>Towards Design Principles of Living Systems</h2>\
+<div class="menu">\
+	<a href="text.html">Text</a>  |  \
+	<a href="#">Fragments</a>  |  \
+	<a href="#">Library</a>  |  \
+	<a href="#">About</a>\
+</div>\
+');
