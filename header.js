50752a9cdabe8738815359d1690b82a565c39508
aplysius <50696341+aplysius@users.noreply.github.com>
2021-09-11 11:02:22 +0200

    Update header.js

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ document.write('\
 <h1>Dancing Matter</h1>\
 <h2>Towards Design Principles of Living Systems</h2>\
 <div class="menu">\
-	<a href="text.html">Text</a>  |  \
+	<a href="text_2.html">Text</a>  |  \
 	<a href="#">Fragments</a>  |  \
 	<a href="#">Library</a>  |  \
 	<a href="#">About</a>\
